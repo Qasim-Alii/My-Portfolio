f84574163309303bfd2237e0ce24ca9e0084971e
Qasim Ali <qasimali.7k@gmail.com>
2025-01-06 00:33:11 +0500

    Initial commit

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -40,7 +40,7 @@ export const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
-    if (document) {
+    if (typeof window !== "undefined") {
       document.body.style.setProperty(
         "--gradient-background-start",
         gradientBackgroundStart
